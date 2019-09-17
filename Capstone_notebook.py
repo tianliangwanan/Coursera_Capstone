6a79fca40face30c97f34d33d61df00f33ffbb22
unknown <tianliangwanan@hotmail.com>
2019-09-17 22:25:18 +0800

    change to .py

---
+++ Capstone_notebook.py
@@ -0,0 +1,51 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Hello Capstone Project Course!\n"
+     ]
+    }
+   ],
+   "source": [
+    "#This notebook will be mainly used for the capstone project\n",
+    "import pandas as pd\n",
+    "import numpy as np\n",
+    "print(\"Hello Capstone Project Course!\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.6.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
